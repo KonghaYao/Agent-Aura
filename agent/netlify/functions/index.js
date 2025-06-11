082c1c04f26ed9d84368140dc045b12792a098c0
江夏尧 <3446798488@qq.com>
2025-06-11 19:43:45 +0800

    chore: 更新 index.js 文件，调整 Server 导入路径并添加注释说明文件将移入 dist 目录

--- agent/netlify/functions/index.js
+++ agent/netlify/functions/index.js
@@ -1,4 +1,5 @@
-import Server from "../../dist/entrypoint.js";
+// 这个文件会移入到dist目录下
+import Server from "../../entrypoint.js";
 export default (req, context) => {
     return Server.fetch(req, { context });
 };
