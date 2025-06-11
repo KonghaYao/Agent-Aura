02b6529fe341af3df64ed34585fd6883b3c87f2a
江夏尧 <3446798488@qq.com>
2025-06-11 20:38:12 +0800

    chore: 更新 @langgraph-js/bundler 依赖至 1.5.2，删除 .netlify 文件夹的忽略规则，并新增 netlify.js 文件以处理请求。

--- agent/public/.netlify/functions/netlify.js
+++ agent/public/.netlify/functions/netlify.js
@@ -1,5 +1,5 @@
 // 这个文件会移入到dist目录下
-import Server from "./entrypoint.js";
+import Server from "../../entrypoint.js";
 export default (req, context) => {
     return Server.fetch(req, { context });
 };
