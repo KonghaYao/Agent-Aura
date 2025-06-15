77bf95b995c6d6136abdc9db7296a119fe73dc96
江夏尧 <3446798488@qq.com>
2025-06-15 09:07:44 +0800

    feat: 更新依赖项 "@langchain/langgraph-sdk" 至 0.0.84 和 "@langgraph-js/api" 至 0.3.2，优化 create_artifacts 工具的描述信息，返回成功消息

--- agent/src/tools/create_artifacts.ts
+++ agent/src/tools/create_artifacts.ts
@@ -4,16 +4,16 @@ import { z } from "zod";
 const ArtifactsSchema = z.object({
     filename: z.string().describe("文件名"),
     filetype: z.string().describe("文件类型"),
-    code: z.string().describe("代码"),
+    code: z.string().describe("代码，without markdown fence block"),
 });
 
 export const create_artifacts = tool(
     async () => {
-        return "saved";
+        return "saved success";
     },
     {
         name: "create_artifacts",
-        description: "",
+        description: "创建一个 Artifacts 文件",
         schema: ArtifactsSchema,
-    }
+    },
 );
