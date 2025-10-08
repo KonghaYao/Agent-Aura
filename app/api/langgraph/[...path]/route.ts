ebfd948741ea514574601308ef35b586e7b97c89
江夏尧 <3446798488@qq.com>
2025-10-08 15:41:48 +0800

    fix: 修复 next.js 垃圾的重复加载问题

--- app/api/langgraph/[...path]/route.ts
+++ app/api/langgraph/[...path]/route.ts
@@ -1,7 +1,23 @@
-import "@/agent/index";
-import {
-    GET,
-    POST,
-    DELETE,
-} from "@langgraph-js/pure-graph/dist/adapter/nextjs/index.js";
-export { GET, POST, DELETE };
+import { NextRequest } from "next/server";
+import { ensureInitialized } from "@langgraph-js/pure-graph/dist/adapter/nextjs/index";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+const registerGraph = async () => {
+    await import("@/agent/index");
+};
+
+export const GET = async (req: NextRequest, context: any) => {
+    const { GET } = await ensureInitialized(registerGraph);
+    return GET(req);
+};
+
+export const POST = async (req: NextRequest, context: any) => {
+    const { POST } = await ensureInitialized(registerGraph);
+    return POST(req);
+};
+
+export const DELETE = async (req: NextRequest, context: any) => {
+    const { DELETE } = await ensureInitialized(registerGraph);
+    return DELETE(req);
+};
